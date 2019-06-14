96af6296b5e404852734bc8e20ebce6ee8a99e4d
Pavan Jadda <jpavanaryan@gmail.com>
2019-06-14 09:51:31 -0400

    Updated

--- src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
+++ src/main/java/com/pj/springdatademo/SpringDataDemoApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataDemoApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(SpringDataDemoApplication.class, args);
     }
-
 }
